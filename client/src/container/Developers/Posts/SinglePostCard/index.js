e85d3c69207e67edf379628f4e5258cfa01f59b9
Jason Dela Torre <jasondelatorre92@gmail.com>
2018-11-02 22:43:31 +0800

    refactor(singlepost) added proptypes

--- client/src/container/Developers/Posts/SinglePostCard/index.js
+++ client/src/container/Developers/Posts/SinglePostCard/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import PropTypes from 'prop-types';
 //Material UI
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
